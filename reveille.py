2ab47fd6b50a06585cb5a5e839d11c6a21e4b619
Rafael Gomez <rafaelgomez.us@gmail.com>
2022-07-03 11:44:29 -0500

    completed register command + mysql DB schema design

--- reveille.py
+++ reveille.py
@@ -22,12 +22,13 @@ EMAIL = config['BOT_EMAIL']
 EMAIL_PASS = secret['BOT_EMAIL_PASS']
 SQL_USER = config['SQL_USER']
 SQL_PASS = secret['SQL_PASS']
-SQL_DB_NAME = config['SQL_DB_NAME']
+DB_NAME = config['SQL_DB_NAME']
+USER_TBL_NAME = config['SQL_USER_TBL_NAME']
 
 
 bot = commands.Bot(command_prefix=PREFIX, help_command=None)
 
-# Sends embed w/ list of commands (command arguments + description)
+# Sends embed w/ list of commands (command syntax, arguments, + description)
 @bot.command()
 async def help(ctx):
     title = '**Command Help Menu**'
@@ -37,6 +38,10 @@ async def help(ctx):
 
     embed = discord.Embed(title=title, description=description, color=color)
     await ctx.send(embed=embed)
+
+# Needs a check  if already verified
+
+
 
 # Initiates registration process w/ email verification (paired w/ verify command for verif completion)
 @bot.command()
@@ -54,29 +59,65 @@ async def register(ctx, net_id):
         verif_code = random.randint(100000, 999999)
         discord_user_id = ctx.message.author.id
 
-        subject = 'Reveille Bot - Identity Verification'
-        body = f'Your verification code is {verif_code}.\n\nIn order to finish verifying, go back to Discord and DM the bot, Reveille, with the following command:\n\n{PREFIX}verify {verif_code}'
-        msg = f'Subject: {subject}\n\n{body}'
+        # Checks for past registration w/ DB discord_user_id val lookup - flags if non-zero matches found
+        is_registered = False
 
         try:
-            db = mysql.connector.connect(host='localhost', user=SQL_USER, passwd=SQL_PASS, database=SQL_DB_NAME)
+            db = mysql.connector.connect(host='localhost', user=SQL_USER, passwd=SQL_PASS, database=DB_NAME)
             cur = db.cursor()
-            cur.execute(f'INSERT INTO users (discord_user_id, net_id, verif_code, is_verif) '
-                        f'VALUES ({discord_user_id}, \'{net_id}\', {verif_code}, 0)')
+            cur.execute('SELECT COUNT(1) '
+                        f'FROM {USER_TBL_NAME} '
+                        f'WHERE discord_user_id = {discord_user_id}')
+            
+            match_num = int(cur.fetchone()[0])
 
-            db.commit()
+            if match_num != 0:
+                is_registered = True
         except Exception as e:
-            await ctx.send(f'Something went wrong. {e}')
+            await ctx.send(f'Something went wrong while checking for past user registration. {e}')
             return
+        
+        # Updates user DB records w/ new verif_code and net_id if is_registered, adds new user record to DB if not
+        if (is_registered):
+            try:
+                db = mysql.connector.connect(host='localhost', user=SQL_USER, passwd=SQL_PASS, database=DB_NAME)
+                cur = db.cursor()
+                cur.execute(f'UPDATE {USER_TBL_NAME} '
+                            f'SET net_id = \'{net_id}\', verif_code = {verif_code} '
+                            f'WHERE discord_user_id = {discord_user_id}')
 
+                db.commit()
+            except Exception as e:
+                await ctx.send(f'Something went wrong while updating user record in database. {e}')
+                return
+        else:
+            try:
+                db = mysql.connector.connect(host='localhost', user=SQL_USER, passwd=SQL_PASS, database=DB_NAME)
+                cur = db.cursor()
+                cur.execute(f'INSERT INTO {USER_TBL_NAME} (discord_user_id, net_id, verif_code, is_verif) '
+                            f'VALUES ({discord_user_id}, \'{net_id}\', {verif_code}, 0)')
+
+                db.commit()
+            except Exception as e:
+                await ctx.send(f'Something went wrong while adding user record to database. {e}')
+                return
+
+        # Makes + sends verification email to user TAMU email address via SMTP
         try:
+            subject = 'Reveille Bot - Identity Verification'
+            body = f'Your verification code is {verif_code}.\n\nIn order to finish verifying, go back to Discord and DM the bot, Reveille, with the following command:\n\n{PREFIX}verify {verif_code}'
+            msg = f'Subject: {subject}\n\n{body}'
+
             smtp.sendmail(sender_email, receiver_email, msg)
-            await ctx.send(f'An email with a verification code has been sent to your TAMU email address ({net_id}@tamu.edu). To confirm your registration and verify, please DM me, Reveille, your verification code in the form of the command `{PREFIX}verify <verif_code>`.\n\nMake sure to check your spam if you\'re having trouble!')
+            if (is_registered):
+                await ctx.send(f'You\'ve already a registered user in the database. An email with a new verification code has been sent to your TAMU email address ({net_id}@tamu.edu). To confirm your registration and verify, please DM me, Reveille, your verification code in the form of the command `{PREFIX}verify <verif_code>`.\n\nMake sure to check your spam if you\'re having trouble!')
+            else:
+                await ctx.send(f'An email with a verification code has been sent to your TAMU email address ({net_id}@tamu.edu). To confirm your registration and verify, please DM me, Reveille, your verification code in the form of the command `{PREFIX}verify <verif_code>`.\n\nMake sure to check your spam if you\'re having trouble!')
         except Exception as e:
-            await ctx.send(f'Something went wrong. {e}')
+            await ctx.send(f'Something went wrong while sending verification email. {e}')
             return
 
-# Verifies users and adds profile to assisting MySQL database
+# Verifies users w/ DB verif_code val lookup comparison, updates is_verif val to 1 if code match
 @bot.command()
 async def verify(ctx, verif_code):
     # to do
