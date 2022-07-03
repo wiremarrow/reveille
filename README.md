723fc89f3a0ffb9a52522d2d38bf068ed4ebbb7a
Rafael Gomez <rafaelgomez.us@gmail.com>
2022-07-03 09:19:38 -0500

    reword, clarify config var, sql connector

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Reveille Bot
 
-Reveille Bot is a local schedule management tool with course directory lookup and school resource/event aggregation capabilities designed for the students of Texas A&M University integrated right into Discord using the [discord.py](https://github.com/Rapptz/discord.py) wrapper.
+Reveille Bot is a local schedule management tool with course directory lookup and school resource/event aggregation capabilities designed for students of Texas A&M University. It is integrated right into Discord using the [discord.py](https://github.com/Rapptz/discord.py) Discord API wrapper.
--- reveille.py
+++ reveille.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import json
 import random
 import smtplib
+import mysql.connector
+
 
 # Initialize config/secret vars
 c = open('config.json')
@@ -18,21 +20,25 @@ PREFIX = config['BOT_COMMAND_PREFIX']
 TOKEN = secret['BOT_TOKEN']
 EMAIL = config['BOT_EMAIL']
 EMAIL_PASS = secret['BOT_EMAIL_PASS']
+SQL_USER = config['SQL_USER']
+SQL_PASS = secret['SQL_PASS']
+SQL_DB_NAME = config['SQL_DB_NAME']
+
 
 bot = commands.Bot(command_prefix=PREFIX, help_command=None)
 
 # Sends embed w/ list of commands (command arguments + description)
 @bot.command()
 async def help(ctx):
-    title = ':wrench: **Command Help Menu**'
-    description = f'''`{PREFIX}help` - Produces this menu for command help.
-    `{PREFIX}register <net_id>` - Register your NetID with the bot to verify yourself.'''
+    title = '**Command Help Menu**'
+    description = (f'`{PREFIX}help` - Produces this menu for command help.\n'
+                   f'`{PREFIX}register <net_id>` - Register your NetID with the bot to verify yourself.')
     color = 0x500000
 
     embed = discord.Embed(title=title, description=description, color=color)
     await ctx.send(embed=embed)
 
-# Initiates registration process w/ email verification (paired w/ verify command for completion)
+# Initiates registration process w/ email verification (paired w/ verify command for verif completion)
 @bot.command()
 async def register(ctx, net_id):
     domain = 'smtp.gmail.com'
@@ -46,12 +52,34 @@ async def register(ctx, net_id):
         sender_email = EMAIL
         receiver_email = f'{net_id}@tamu.edu'
         verif_code = random.randint(100000, 999999)
+        discord_user_id = ctx.message.author.id
 
         subject = 'Reveille Bot - Identity Verification'
-        body = f'Your verification code is {verif_code}.\n\nIn order to finish verifying, go back to Discord and DM the bot, Reveille, with the following command:\n\n&verify {verif_code}'
+        body = f'Your verification code is {verif_code}.\n\nIn order to finish verifying, go back to Discord and DM the bot, Reveille, with the following command:\n\n{PREFIX}verify {verif_code}'
         msg = f'Subject: {subject}\n\n{body}'
 
-        smtp.sendmail(sender_email, receiver_email, msg)
-    await ctx.send(f'An email has been sent to your TAMU email address ({net_id}@tamu.edu). To confirm your registration through verification, please DM `&verify <code>` to me, Reveille, with the code you were emailed.')
+        try:
+            db = mysql.connector.connect(host='localhost', user=SQL_USER, passwd=SQL_PASS, database=SQL_DB_NAME)
+            cur = db.cursor()
+            cur.execute(f'INSERT INTO users (discord_user_id, net_id, verif_code, is_verif) '
+                        f'VALUES ({discord_user_id}, \'{net_id}\', {verif_code}, 0)')
+
+            db.commit()
+        except Exception as e:
+            await ctx.send(f'Something went wrong. {e}')
+            return
+
+        try:
+            smtp.sendmail(sender_email, receiver_email, msg)
+            await ctx.send(f'An email with a verification code has been sent to your TAMU email address ({net_id}@tamu.edu). To confirm your registration and verify, please DM me, Reveille, your verification code in the form of the command `{PREFIX}verify <verif_code>`.\n\nMake sure to check your spam if you\'re having trouble!')
+        except Exception as e:
+            await ctx.send(f'Something went wrong. {e}')
+            return
+
+# Verifies users and adds profile to assisting MySQL database
+@bot.command()
+async def verify(ctx, verif_code):
+    # to do
+    await ctx.send('You\'re successfully verified! You now have full server/bot functionality.')
 
 bot.run(TOKEN)
